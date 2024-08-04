e1880e607f7c3222cb5f3aff5265acaa921c340d
najat <najat.najat23@ump.ac.ma>
2024-08-04 15:27:35 +0100

    HOME

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,26 @@
-import React, {useEffect,useState} from 'react'
-import Login from './login'
-function App() {
+import Home from 'Home';
+import Header from 'Header';
+ import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';;
+import AddUser from './addUser';
+import Login from './login';
+import Gestion from './Gestion_user';
 
-  const [ backendData, setBackendData] = useState([{}])
-  useEffect(() => {
-    fetch("/api").then(
-      Response => Response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-    }
-  )
-  }, [])
+
+function App() {
   return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>
-          <Login/>
-        </p>
-      ): (
-        backendData.users.map((user,i) => (
-          <p key={i}>{user}</p>
-        ))
-      )
-      } 
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login/>} />
+          <Route path="/addUser" element={<AddUser/>} />
+          <Route path="/gestion" element={<Gestion/>} />
+      </Routes>
+    </Router>
   );
 }
+
+
+
 export default App;
--- client/src/login.js
+++ client/src/login.js
@@ -1,26 +1,94 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import validation from "./loginvalidation";
+import axios from "axios";
+import "./login.css";
 
-const Login = () => {
-    return (
-    <div className='d-flex justify-content-center'>
-            <div className='bg-white '>
-                <form action="">
-                    <div className='mb-3'>
-                        <label htmlFor="email">Email</label><br></br>
-                        <input type="email" placeholder='Email' name="email" id="email" />
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="password">Mot de passe</label><br></br>
-                        <input type="password" placeholder='Mot de passe' name="password" id="password" />
-                    </div>
-                    <p>Mot de passe oublié ?</p>
-                    <button className='btn_btn_success'>Se connecter</button>
-                    <button className='btn_btn_default_border'>S'inscrire</button>
-                </form>
-                {/* Your login component JSX goes here */}
-            </div>
+function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+    if (errors.password === "" && errors.email === "") {
+      axios
+        .post("http://localhost:3001/login", values)
+        .then((res) => {
+          if (res.data === "success") {
+            navigate("/");
+          } else {
+            alert("no record");
+          }
+          console.log("success");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  return (
+    <div className="back">
+      <div className="header">
+        <div className="logo">
+          <img srcSet="src/logo.png"/>
+          <div className="div-4">ParcAuto</div>
         </div>
-    );
-};
+        <div className="di">
+       <div>Accueil</div>
+       <div>Services</div>
+       <div>A propos</div>
+       <div>Contact</div>        
+    </div>
+      </div>
+      <div className="box ">
+        <form action="" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <br></br>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              id="email"
+              onChange={handleInput}
+            />
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Mot de passe</label>
+            <br></br>
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              id="password"
+              onChange={handleInput}
+            />
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+          </div>
+          <p style={{ fontSize: "12px" }}>Mot de passe oublié ?</p>
+          <button type="submit" className="btn btn-success">
+            Se connecter
+          </button>
+          <Link to="/addUser" className="btn btn-success">
+            ajouter
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
